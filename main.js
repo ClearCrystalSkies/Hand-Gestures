cbbaadd6c38d9604f590fd429ccce191e2045d45
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2023-05-22 19:00:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
-//js code for webcam properties
 Webcam.set({
     width: 360,
     height: 250,
     image_format: 'png',
     png_quality : 90
 });
-//js code for trigerring webcam
 camera = document.getElementById("camera");
-Webcam.attach("#camera");
-// function to take snapshot
+Webcam.attach('#camera');
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-dpvIoK4A/model.json", modelLoaded);
+function modelLoaded(){
+  console.log("Model has been loaded!");
+}
 function take_snapshot(){
     Webcam.snap(function(data_uri){
     document.getElementById("captured_img").innerHTML= '<img id="selfie_image" src="'+ data_uri +'">';
   })
 };
-//consoling ml5 version
-console.log("ml5 version", ml5.version);
-//importing the model link of teachable machine; model.json
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/-dpvIoK4A/model.json", modelLoaded);
-function modelLoaded(){
-  console.log("Model has been loaded!");
+function identify(){
+  img = document.getElementById('captured_img');
+  classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+  if(error){
+    console.log(error);
+  }else{
+    console.log(results);
+    speak();
+  };
+}
+function speak(){
+  if (results[0].label == "Good") {
+    symbol = "Good";
+    document.getElementsById("results_tag").innerHTML = "👍"
+  }
+  if (results[0].label == "Nice") {
+    symbol = "Nice";
+    document.getElementsById("results_tag").innerHTML = "&#128076;";
+  }
+  if (results[0].label == "Wait") {
+    symbol = "Wait";
+    document.getElementsById("results_tag").innerHTML = "✋"
+  }
+  if (results[0].label == "Victory") {
+    symbol = "Victory";
+    document.getElementsById("results_tag").innerHTML = "&#128071;";
+  }
+  if (results[0].label == "Rock on!") {
+    symbol = "Rock on!";
+    document.getElementsById("results_tag").innerHTML = "🤘"
+  }
+  if (results[0].label == "Spock") {
+    symbol = "Spock";
+    document.getElementsById("results_tag").innerHTML = "&#128406;";
+  }
+  var synth = window.speechSynthesis;
+  var speak_data = "The symbol predicted is " + symbol;
+  var utterThis = SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis);
 }
